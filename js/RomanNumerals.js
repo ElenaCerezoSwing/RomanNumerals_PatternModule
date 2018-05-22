890eb2dcfb3eb1c9b19dfb35500d2de3c038429c
Ele_hopper <elenacerezo83@gmail.com>
2018-05-22 17:35:38 +0200

    transform exception to a while loop

--- js/RomanNumerals.js
+++ js/RomanNumerals.js
@@ -1,12 +1,17 @@
 var RomanNumerals = function () { };
 
+var numerals = [
+    { value: 4, numeral: "IV" },
+    { value: 1, numeral: "I" }
+]
 RomanNumerals.prototype.fromNumber = function (n) {
-    if (3 === n) {
-        return "III";
-    }
-    if (2 === n) {
-        return "II";
-    }
-    return "I";
+    var result = "";
+    numerals.forEach(function (item) {
+        while (n >= item.value) {
+            result += item.numeral;
+            n -= item.value;
+        }
+    });
+    return result;
 };
 module.exports = new RomanNumerals();
