350e1eaafd1e1da06b1435d55e6e30d54a225fc1
Ele_hopper <elenacerezo83@gmail.com>
2018-05-23 10:19:52 +0200

    solved pattern module

--- js/RomanNumerals.js
+++ js/RomanNumerals.js
@@ -1,27 +1,37 @@
-var RomanNumerals = function () { };
+function Application() {
 
-var numerals = [
-    { value: 1000, numeral: "M" },
-    { value: 500, numeral: "D" },
-    { value: 100, numeral: "C" },
-    { value: 99, numeral: "IC" },
-    { value: 50, numeral: "L" },
-    { value: 49, numeral: "IL" },
-    { value: 40, numeral: "XL" },
-    { value: 10, numeral: "X" },
-    { value: 9, numeral: "IX" },
-    { value: 5, numeral: "V" },
-    { value: 4, numeral: "IV" },
-    { value: 1, numeral: "I" }
-]
-RomanNumerals.prototype.fromNumber = function (n) {
-    var result = "";
-    numerals.forEach(function (item) {
-        while (n >= item.value) {
-            result += item.numeral;
-            n -= item.value;
-        }
-    });
-    return result;
-};
-module.exports = new RomanNumerals();
+    var numerals = [
+        { value: 1000, numeral: "M" },
+        { value: 500, numeral: "D" },
+        { value: 100, numeral: "C" },
+        { value: 99, numeral: "IC" },
+        { value: 50, numeral: "L" },
+        { value: 49, numeral: "IL" },
+        { value: 40, numeral: "XL" },
+        { value: 10, numeral: "X" },
+        { value: 9, numeral: "IX" },
+        { value: 5, numeral: "V" },
+        { value: 4, numeral: "IV" },
+        { value: 1, numeral: "I" }
+    ];
+
+    function fromNumber(n) {
+        var result = "";
+        numerals.forEach(function (item) {
+            while (n >= item.value) {
+                result += item.numeral;
+                n -= item.value;
+            }
+        });
+        return result;
+    };
+
+    return {
+        fromNumber: fromNumber
+    }
+
+}
+
+if (typeof (module) != undefined) {
+    module.exports = Application;
+}
--- js/spec.js
+++ js/spec.js
@@ -1,12 +1,18 @@
-describe("Roman Numerals", function () {
-    it("convert numbers to roman numerals", function () {
-        var romanNumerals = require("../js/RomanNumerals");
+const Application = require("../js/RomanNumerals");
+let app;
 
-        expect(romanNumerals.fromNumber(1)).toBe("I");
-        expect(romanNumerals.fromNumber(2)).toBe("II");
-        expect(romanNumerals.fromNumber(3)).toBe("III");
-        expect(romanNumerals.fromNumber(4)).toBe("IV");
-        expect(romanNumerals.fromNumber(7777)).toBe("MMMMMMMDCCLXXVII");
+beforeEach(function () {
+    app = Application();
+});
+
+describe("Roman Numerals", function () {
+
+    it("convert numbers to roman numerals", function () {
+        expect(app.fromNumber(1)).toBe("I");
+        expect(app.fromNumber(2)).toBe("II");
+        expect(app.fromNumber(3)).toBe("III");
+        expect(app.fromNumber(4)).toBe("IV");
+        expect(app.fromNumber(7777)).toBe("MMMMMMMDCCLXXVII");
     });
 
 });
